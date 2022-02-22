d456c6591c4296fd236d26d3bd74a4ceaacfa37a
Adriano <adrianosantosr1@gmail.com>
2022-02-22 20:10:42 -0300

    I created the POST and PUT route

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const uuid = require("uuid");
+
 const port = 3000;
 const app = express();
 app.use(express.json());
@@ -7,6 +9,31 @@ const users = [];
 
 app.get("/users", (request, response) => {
   return response.json(users);
+});
+
+app.post("/users", (request, response) => {
+  const { name, age } = request.body;
+
+  const user = { id: uuid.v4(), name, age };
+
+  users.push(user);
+
+  return response.status(201).json(user);
+});
+
+app.put("/users/:id", (request, response) => {
+  const { id } = request.params;
+  const { name, age } = request.body;
+
+  const updateUser = { id, name, age };
+
+  const index = users.findIndex((user) => user.id === id);
+  if (index < 0) {
+    return response.status(404).json({ message: "user not found" });
+  }
+
+  users[index] = updateUser;
+  return response.json(updateUser);
 });
 
 app.listen(3000, () => {
